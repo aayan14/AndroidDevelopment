01a93ab3c3dcad4615c355e56fd71c7019f6552a
Aayan Singh <aayansingh806@gmail.com>
2021-07-15 18:17:29 +0530

    Lucky Roll: Dice game

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,35 +14,44 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener{
-            val luckynum = 4
-            rollDice(luckynum)
+            rollDice()
 
         }
     }
 
-    private fun rollDice(luckyNum: Int) {
-        val dice = Dice(6)
-        val returnImageView: ImageView = findViewById(R.id.imageView)
+    private fun rollDice() {
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+        val returnImageView1: ImageView = findViewById(R.id.imageView1)
         val returnImageView2: ImageView = findViewById(R.id.imageView2)
         val returnTextView: TextView = findViewById(R.id.textView)
-        val randomNum = dice.roll()
-        when(randomNum) {
-            1 -> returnImageView.setImageResource(R.drawable.dice_1)
-            2 -> returnImageView.setImageResource(R.drawable.dice_2)
-            3 -> returnImageView.setImageResource(R.drawable.dice_3)
-            4 -> returnImageView.setImageResource(R.drawable.dice_4)
-            5 -> returnImageView.setImageResource(R.drawable.dice_5)
-            6 -> returnImageView.setImageResource(R.drawable.dice_6)
+        val randomNum1 = dice1.roll()
+        val randomNum2 = dice2.roll()
+        when(randomNum1) {
+            1 -> returnImageView1.setImageResource(R.drawable.dice_1)
+            2 -> returnImageView1.setImageResource(R.drawable.dice_2)
+            3 -> returnImageView1.setImageResource(R.drawable.dice_3)
+            4 -> returnImageView1.setImageResource(R.drawable.dice_4)
+            5 -> returnImageView1.setImageResource(R.drawable.dice_5)
+            6 -> returnImageView1.setImageResource(R.drawable.dice_6)
+        }
+        when(randomNum2) {
+            1 -> returnImageView2.setImageResource(R.drawable.dice_1)
+            2 -> returnImageView2.setImageResource(R.drawable.dice_2)
+            3 -> returnImageView2.setImageResource(R.drawable.dice_3)
+            4 -> returnImageView2.setImageResource(R.drawable.dice_4)
+            5 -> returnImageView2.setImageResource(R.drawable.dice_5)
+            6 -> returnImageView2.setImageResource(R.drawable.dice_6)
         }
 
-        if (randomNum == luckyNum){
-            returnTextView.text = "Congrats!!! Its the lucky number."
-            returnImageView2.setImageResource(R.color.green)
+        if (randomNum1 + randomNum2 == 6){
+            returnTextView.text = "Congrats!!! You are out of jail."
+            returnTextView.setBackgroundColor(resources.getColor(R.color.green))
 
         } else {
-            returnTextView.text = "Sorry!!! Better luck next time."
+            returnTextView.text = "Sorry!!! You are still in jail."
+            returnTextView.setBackgroundColor(resources.getColor(R.color.white))
 
-            returnImageView2.setImageResource(R.color.red)
         }
     }
 }
